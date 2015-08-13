a491eb665b2bfc5f8d0df463687a5d0c3a968ebe
atmilich <alexandra.tmilich@gmail.com>
2015-08-13 18:53:12 +0000

    SO CLOSE

--- js/layout.js
+++ js/layout.js
@@ -1,3 +1,18 @@
-$( "#sanders" ).mouseover(function() {
-  $("#s_name").css("display", "block");
+$(document).ready(function(){
+  
+  $('#MyModal').modal('hide');
+    
+    $("#abortion").click(function(){
+    console.log("let's try this");
+    $('.abortion-title').show();
+    $('#trumpAbort').show();
+    
+  });      
+
+//    $("#education").click(function(){
+//     console.log("let's try this");
+//     $('.education-title').show();
+//     $('.education-body').show();
+    
+//   });
 });
