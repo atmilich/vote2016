9334962cf882474e9ca8973f8d356f5475030f25
atmilich <alexandra.tmilich@gmail.com>
2015-08-14 02:29:38 +0000

    table

--- js/layout.js
+++ js/layout.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
-  
-  $('#MyModal').modal('hide');
-    
-    $("#abortion").click(function(){
-    console.log("let's try this");
-    $('.abortion-title').show();
-    $('#trumpAbort').show();
-    
-  });      
+var cols = ['Abort', 'Education', 'Immigration', 'Gun', 'Health', 'Gay'] 
+var cands = ['sanders', 'clinton', 'omalley', 'rubio', 'bush', 'trump']
 
-//    $("#education").click(function(){
-//     console.log("let's try this");
-//     $('.education-title').show();
-//     $('.education-body').show();
-    
-//   });
+$(document).ready(function(){
+//   $('#MyModal').modal('hide');   
+    $("#abortion").click(function(){
+      console.log("let's try this");
+      $('.abortion-title').show();
+      for (i = 0; i < cols.length; i++) { 
+        for (ii = 0; ii < cands.length; ii++) { 
+          console.log('.' + cands[ii] + cols[i]); 
+           if(cols[i].indexOf("Abort") == -1){
+            $('.' + cands[ii] + cols[i]).hide(); 
+           }
+           else {
+             console.log("Is abortion.")
+             $('.' + cands[ii] + cols[i]).show(); 
+           }
+        }
+      }
+    });  
 });
